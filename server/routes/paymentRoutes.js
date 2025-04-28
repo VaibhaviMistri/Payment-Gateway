5cfed2af077a1267be7d187caae71f25a606668e
Vaibhavi <vaibhavi.mce20@sot.pdpu.ac.in>
2025-04-28 17:04:52 +0530

    Temporary folder rename commit

--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { createOrder } from '../controllers/createOrderController.js';
-import { validatePayment } from '../controllers/validatePaymentController.js';
-
-router.route("/order").post(createOrder);
-router.route("/order/validate").post(validatePayment);
-
-export default router;
